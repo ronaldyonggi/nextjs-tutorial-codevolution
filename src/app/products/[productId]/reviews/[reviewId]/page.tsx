16daa41da05cdd0b14fc12e28fca92770b30f816
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-03 11:54:29 +0700

    comment out deliberate review error code

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -8,15 +8,15 @@ interface ReviewDetailsProps {
   }
 }
 
-function getRandomInt(count: number) {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
 
 export default function ReviewDetails({ params }: ReviewDetailsProps) {
-  const random = getRandomInt(2)
-  if (random === 1) {
-    throw new Error("Error loading review")
-  }
+  // const random = getRandomInt(2)
+  // if (random === 1) {
+  //   throw new Error("Error loading review")
+  // }
 
   if (parseInt(params.reviewId) > 1000) notFound();
   return (
